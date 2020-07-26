9ee47523a69a7f02e325f66a276b6c7c69e38709
Prabu M <43335779+prabuvimal2000@users.noreply.github.com>
2020-07-26 17:09:46 +0530

    Create BinarytoDec.java

---
+++ BinarytoDec.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BinarytoDec
+{  
+ public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+        int num =sc.nextInt();
+        int rem=0,base=1,dec_value=0;
+        while(num>0)
+        {
+            rem = num%10; 
+            num/=10; 
+            dec_value+=rem*base;
+            base*=2;
+        }
+        System.out.println(dec_value);
+    }
+}
