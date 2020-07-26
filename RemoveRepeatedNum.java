0f69329066f17fd22e620be0006bd6f3d08c50aa
Prabu M <43335779+prabuvimal2000@users.noreply.github.com>
2020-07-26 19:58:49 +0530

    Create RemoveRepeatedNum.java

---
+++ RemoveRepeatedNum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class RemoveRepeatedNum
+{
+     static long removeRecur(long n)
+     {
+        long prev_digit = n % 10; 
+        long pow = 10; 
+        long res = prev_digit; 
+        while (n>0) { 
+            long curr_digit = n % 10; 
+      
+            if (curr_digit != prev_digit)  
+            { 
+                res += curr_digit * pow; 
+                prev_digit = curr_digit; 
+                pow *= 10; 
+            } 
+            n = n / 10; 
+        } 
+       return res;
+     }  
+      public static void main(String[] args)
+      {
+         Scanner sc = new Scanner(System.in);
+         long n = sc.nextLong();
+         System.out.println(removeRecur(n));     
+    }
+}
+      
