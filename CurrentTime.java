f029871b56cc23e01fa6db99615434261838c710
Prabu M <43335779+prabuvimal2000@users.noreply.github.com>
2020-08-02 15:23:14 +0530

    Create CurrentTime.java

---
+++ CurrentTime.java
@@ -0,0 +1,9 @@
+import java.time.LocalTime;
+public class CurrentTime
+{
+  public static void main(String[] args) 
+  {
+    LocalTime myObj = LocalTime.now();
+    System.out.println(myObj);
+  }
+}
